dc699f60b788e537280141d2a9dcdb86497cbd8e
gaohw <ghw0614@126.com>
2018-04-13 00:08:46 +0800

    新增生成二维码工具类

--- src/main/test/TestYou.java
+++ src/main/test/TestYou.java
@@ -1,7 +1,11 @@
+import com.google.zxing.WriterException;
 import com.grow.utils.StringExUtil;
+import com.grow.utils.qrcode.QrCodeUtil;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * FileName:TestYou
@@ -21,5 +25,12 @@ public class TestYou {
         logger.info("info测试过程中--");
         logger.warn("warn会出来吗");
         logger.error("error");
+    }
+
+    @Test
+    public void testQrCode() throws IOException, WriterException {
+        logger.info("测试二维码工具类");
+
+        QrCodeUtil.makeEncode();
     }
 }
