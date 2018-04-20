884b31e8b57df96678aceda07207292d718c2a3c
gaohw <ghw0614@126.com>
2018-04-20 17:18:08 +0800

    整合注解redis

--- src/main/java/com/grow/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/grow/service/impl/CategoryServiceImpl.java
@@ -2,7 +2,11 @@ package com.grow.service.impl;
 
 import com.grow.dao.CategoryMapper;
 import com.grow.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,13 +19,20 @@ import java.util.Map;
  * Description: 实现业务层接口
  */
 @Service
+@CacheConfig(cacheNames = "sysCategory")
 public class CategoryServiceImpl implements CategoryService {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     CategoryMapper categoryMapper;
 
     @Override
+    @Cacheable
     public List<Map<String, Object>> list() {
+        for (Map<String, Object> map : categoryMapper.list()) {
+            logger.info("查询结果：" + map.get("name"));
+        }
         return categoryMapper.list();
     }
 }
